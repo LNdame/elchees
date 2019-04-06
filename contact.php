a81faa0fc8c05c19415b0ab20cb0c02d02c07454
LNdame <ls20045@gmail.com>
2019-04-06 16:46:35 +0200

    init commit

---
+++ contact.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <!-- The above 4 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+
+    <!-- Title -->
+    <title>ELCHEES - School for Computer, Management & Secretarial Courses</title>
+
+    <!-- Favicon -->
+    <link rel="icon" href="img/core-img/favicon.ico">
+
+    <!-- Core Stylesheet -->
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+    <!-- ##### Preloader ##### -->
+    <div id="preloader">
+        <i class="circle-preloader"></i>
+    </div>
+
+    <!-- ##### Header Area Start ##### -->
+    <?php include('includes/header.php') ?>
+    <!-- ##### Header Area End ##### -->
+
+    <!-- ##### Breadcumb Area Start ##### -->
+    <div class="breadcumb-area bg-img" style="background-image: url(img/bg-img/breadcumb.jpg);">
+        <div class="bradcumbContent">
+            <h2>Contact</h2>
+        </div>
+    </div>
+    <!-- ##### Breadcumb Area End ##### -->
+
+    <!-- ##### Google Maps ##### -->
+    <div class="map-area wow fadeInUp" data-wow-delay="300ms">
+       <!-- <div id="googleMap"></div> -->
+        <div>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3398.4971936746206!2d28.788774314873613!3d-31.592833411969526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e5fd0954bb8ad65%3A0x31109786d0d9204c!2s52+Sprigg+St%2C+Norwood%2C+Mthatha%2C+5100!5e0!3m2!1sen!2sza!4v1554560249542!5m2!1sen!2sza" width="1920" height="600" frameborder="0" style="border:0" allowfullscreen></iframe>  
+        </div>
+    </div>
+
+    <!-- ##### Contact Area Start ##### -->
+    <section class="contact-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="contact-content">
+                        <div class="row">
+                            <!-- Contact Information -->
+                            <div class="col-12 col-lg-5">
+                                <div class="contact-information wow fadeInUp" data-wow-delay="400ms">
+                                    <div class="section-heading text-left">
+                                        <span>The Best</span>
+                                        <h3>Contact Us</h3>
+                                        <p class="mt-30">use the details below to get in touch with us</p>
+                                    </div>
+
+                                    <!-- Contact Social Info -->
+                                    <div class="contact-social-info d-flex mb-30">
+                                        <a href="#"><i class="fa fa-pinterest" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-dribbble" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-behance" aria-hidden="true"></i></a>
+                                        <a href="#"><i class="fa fa-linkedin" aria-hidden="true"></i></a>
+                                    </div>
+
+                                    <!-- Single Contact Info -->
+                                    <div class="single-contact-info d-flex">
+                                        <div class="contact-icon mr-15">
+                                            <i class="icon-placeholder"></i>
+                                        </div>
+                                        <p> No. 52 Sprigg Street,	
+                                            Office 205 & 206, ECRDA Building <br>
+                                            Mthatha 5099 </p>
+                                    </div>
+
+                                    <!-- Single Contact Info -->
+                                    <div class="single-contact-info d-flex">
+                                        <div class="contact-icon mr-15">
+                                            <i class="icon-telephone-1"></i>
+                                        </div>
+                                        <p>Tel No.   047 532 2339 <br>
+                                            Cell No. 083 694 2907 <br>
+                                        Fax No. 086 263 2603 <br>
+                                        Tel No.(after hrs.) 047 531 4747
+                                            </p>
+                                    </div>
+
+                                    <!-- Single Contact Info -->
+                                    <div class="single-contact-info d-flex">
+                                        <div class="contact-icon mr-15">
+                                            <i class="icon-contract"></i>
+                                        </div>
+                                        <p>elchees@telkomsa.net</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Contact Form Area -->
+                            <div class="col-12 col-lg-7">
+                                <div class="contact-form-area wow fadeInUp" data-wow-delay="500ms">
+                                    <form action="#" method="post">
+                                        <input type="text" class="form-control" id="name" placeholder="Name">
+                                        <input type="email" class="form-control" id="email" placeholder="E-mail">
+                                        <textarea name="message" class="form-control" id="message" cols="30" rows="10" placeholder="Message"></textarea>
+                                        <button class="btn academy-btn mt-30" type="submit">Contact Us</button>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### Contact Area End ##### -->
+
+   
+   <!-- ##### Footer Area Start ##### -->
+   <?php include('includes/footer.php') ?>
+    <!-- ##### Footer Area Start ##### -->
+    
+
+    <!-- ##### All Javascript Script ##### -->
+    <!-- jQuery-2.2.4 js -->
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <!-- Popper js -->
+    <script src="js/bootstrap/popper.min.js"></script>
+    <!-- Bootstrap js -->
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <!-- All Plugins js -->
+    <script src="js/plugins/plugins.js"></script>
+    <!-- Active js -->
+    <script src="js/active.js"></script>
+    <!-- Google Maps -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAwuyLRa1uKNtbgx6xAJVmWy-zADgegA2s"></script>
+    <script src="js/google-map/map-active.js"></script>
+</body>
+
+</html>
---
+++ includes/header.php
@@ -0,0 +1,101 @@
+<header class="header-area">
+
+<!-- Top Header Area -->
+<div class="top-header">
+    <div class="container h-100">
+        <div class="row h-100">
+            <div class="col-12 h-100">
+                <div class="header-content h-100 d-flex align-items-center justify-content-between">
+                    <div class="academy-logo">
+                        <a href="index.php"><img src="img/core-img/logo.png" alt=""></a>
+                    </div>
+                    <div class="login-content">
+                        <a href="#">Register / Login</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Navbar Area -->
+<div class="academy-main-menu">
+    <div class="classy-nav-container breakpoint-off">
+        <div class="container">
+            <!-- Menu -->
+            <nav class="classy-navbar justify-content-between" id="academyNav">
+
+                <!-- Navbar Toggler -->
+                <div class="classy-navbar-toggler">
+                    <span class="navbarToggler"><span></span><span></span><span></span></span>
+                </div>
+
+                <!-- Menu -->
+                <div class="classy-menu">
+
+                    <!-- close btn -->
+                    <div class="classycloseIcon">
+                        <div class="cross-wrap"><span class="top"></span><span class="bottom"></span></div>
+                    </div>
+
+                    <!-- Nav Start -->
+                    <div class="classynav">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="#">Pages</a>
+                                <ul class="dropdown">
+                                    <li><a href="index.php">Home</a></li>
+                                    <li><a href="about-us.php">About Us</a></li>
+                                    <li><a href="course.php">Course</a></li>
+                                    <li><a href="blog.html">Blog</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                    <li><a href="elements.html">Elements</a></li>
+                                </ul>
+                            </li>
+                            <!--<li><a href="#">Mega Menu</a>
+                                <div class="megamenu">
+                                    <ul class="single-mega cn-col-4">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Services &amp; Features</a></li>
+                                        <li><a href="#">Accordions and tabs</a></li>
+                                        <li><a href="#">Menu ideas</a></li>
+                                        <li><a href="#">Students Gallery</a></li>
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Services &amp; Features</a></li>
+                                        <li><a href="#">Accordions and tabs</a></li>
+                                        <li><a href="#">Menu ideas</a></li>
+                                        <li><a href="#">Students Gallery</a></li>
+                                    </ul>
+                                    <ul class="single-mega cn-col-4">
+                                        <li><a href="#">Home</a></li>
+                                        <li><a href="#">Services &amp; Features</a></li>
+                                        <li><a href="#">Accordions and tabs</a></li>
+                                        <li><a href="#">Menu ideas</a></li>
+                                        <li><a href="#">Students Gallery</a></li>
+                                    </ul>
+                                    <div class="single-mega cn-col-4">
+                                        <img src="img/bg-img/bg-1.jpg" alt="">
+                                    </div>
+                                </div>
+                            </li> -->
+                            <li><a href="about-us.php">About Us</a></li>
+                            <li><a href="course.php">Course</a></li>
+                            <li><a href="contact.php">Contact</a></li>
+                        </ul>
+                    </div>
+                    <!-- Nav End -->
+                </div>
+
+                <!-- Calling Info -->
+                <div class="calling-info">
+                    <div class="call-center">
+                        <a href="tel:+27475322339 "><i class="icon-telephone-2"></i> <span>(+27) 047 532 2339</span></a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+</div>
+</header>
